f47e30ec0bf189625927d2675ea211f928737afe
Jacob Persson <jacob.persson@wire.com>
2017-10-19 17:39:13 +0200

    Bump version and apply hotfix for duplicate client

--- Source/Synchronization/ZMHotFixDirectory.m
+++ Source/Synchronization/ZMHotFixDirectory.m
@@ -120,6 +120,11 @@ static NSString* ZMLogTag ZM_UNUSED = @"HotFix";
                      patchWithVersion:@"97.2.0" patchCode:^(NSManagedObjectContext *moc) {
                          [ZMHotFixDirectory deleteDuplicatedClientsIn:moc];
                      }],
+                    
+                    [ZMHotFixPatch
+                     patchWithVersion:@"122.0.4" patchCode:^(NSManagedObjectContext *moc) {
+                         [ZMHotFixDirectory deleteDuplicatedClientsIn:moc];
+                     }],
                     ];
     });
     return patches;
